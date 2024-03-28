4ef8a49cca275dd63cd62f6027370c2dad64dc33
Dan Jacoby <jacobydan01@gmail.com>
2024-03-28 19:59:19 -0400

    annotedVersion

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+//function that iterates over element of tutorials and applies a function to it
+//in this case tutorial is a parameter which is each individual element in the array
+const titleCased = () => tutorials.map((tutorial) =>
+//.split takes each element and splits it up based upon the space
+//basically each word is its own element
+//then it maps each word....  
+  tutorial.split(' ').map((word) =>
+  //maps it where the first letter of each word is upper cased, and the rest of the word is added
+    word.charAt(0).toUpperCase() + word.slice(1)
+    //then it joins it back together with a space added back
+  ).join(' ')
+)
 
-const titleCased = () => {
-  return tutorials.map((tutorial) => {
-    return tutorial.split(' ').map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1)
-      
-    }).join(' ')
-  }
-  
-  )
-}
+
